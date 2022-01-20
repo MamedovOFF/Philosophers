b23544191cff1965b11df1ab524005e7d8564ddf
Svetlana Purple <spurple@et-n4.kzn.21-school.ru>
2022-01-20 22:00:27 +0300

    V-4

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include "philo.h"
 
-
 long	get_time(long i)
 {
 	long			time;
@@ -12,16 +11,18 @@ long	get_time(long i)
 	return (time + i);
 }
 
-
 void	philo_init(t_table *table, char **argv, int flag)
 {
 	int	i;
 
 	i = 0;
 	table->philo = malloc(sizeof(t_philo) * table->all_philo);
+	table->forks = malloc(sizeof(pthread_mutex_t) * table->all_philo);
 	while (i < table->all_philo)
 	{
 		table->philo[i] = malloc(sizeof(t_philo));
+		pthread_mutex_init(&table->forks[i], NULL);
+		table->philo[i]->fork = table->forks[i];
 		table->philo[i]->philo_num = i + 1;
 		table->philo[i]->flag = 0;
 		table->philo[i]->time_die = ft_atoi(argv[2]);
@@ -29,42 +30,44 @@ void	philo_init(t_table *table, char **argv, int flag)
 		table->philo[i]->time_sleep = ft_atoi(argv[4]);
 		if (flag == 6)
 			table->philo[i]->nte = ft_atoi(argv[5]);
-		pthread_mutex_init(&table->philo[i]->forks, NULL);
 		i++;
 	}
-
 }
-
 
 void	*philo_life(void* arg)
 {
 	t_philo	*philo;
-	long die;
-	long eating;
+	long	die;
 
 	philo = (t_philo *)arg;
 	philo->start = get_time(0);
 	die = get_time(philo->time_die);
-	eating = get_time(philo->time_eat);
-	printf("Tread %d\n", philo->philo_num);
 	while (1)
 	{
+		printf("%ld %d is has taken a fork\n", get_time(0) - philo->start,philo->philo_num);
+		printf("%ld %d is eating\n", get_time(0) - philo->start, philo->philo_num);
+		pthread_mutex_lock(&philo->fork);
+		usleep(philo->time_eat * 1000);
+		pthread_mutex_unlock(&philo->fork);
+		printf("%ld %d is sleeping\n", get_time(0) - philo->start, philo->philo_num);
+		usleep(philo->time_sleep * 1000);
+		printf("%ld %d is thinking\n", get_time(0) - philo->start, philo->philo_num);
 		if (get_time(0) > die )
 			philo->flag = 1;
+		usleep(100);
 	}
-	pthread_mutex_lock(&philo->forks);
-	pthread_mutex_unlock(&philo->forks);
 	return NULL;
 }
 
 void	pthread_run(t_philo *philo)
 {
 	pthread_create(&philo->tid, NULL, philo_life, (void *)philo);
+	pthread_detach(philo->tid);
 }
 
-void monitoring(t_table *table)
+void	monitoring(t_table *table)
 {
-	int i;
+	int	i;
 
 	while (1)
 	{
@@ -73,13 +76,13 @@ void monitoring(t_table *table)
 		{
 			if (table->philo[i]->flag == 1)
 			{
-				printf("Philo %d dead\n", table->philo[i]->philo_num);
+				printf("%ld %d died\n", get_time(0) - table->philo[i]->start,
+					   table->philo[i]->philo_num);
 				exit(0);
 			}
 			i++;
 		}
 	}
-	
 }
 
 int	main(int argc, char **argv)
--- philo.h
+++ philo.h
@@ -7,21 +7,22 @@
 
 typedef struct s_philo
 {
-	int		philo_num;
-	long		start;
-	int		flag;
-	int		time_die;
-	int		time_sleep;
-	int		time_eat;
-	int		time_fork;
-	pthread_t	tid;
-	pthread_mutex_t	forks;
-	int		nte;
+	int				philo_num;
+	long			start;
+	int				flag;
+	int				time_die;
+	int				time_sleep;
+	int				time_eat;
+	int				time_fork;
+	pthread_mutex_t	fork;
+	pthread_t		tid;
+	int				nte;
 }	t_philo;
 
 typedef struct s_table
 {
-	int			all_philo;
-	t_philo		**philo;
+	int				all_philo;
+	pthread_mutex_t	*forks;
+	t_philo			**philo;
 }	t_table;
 #endif
